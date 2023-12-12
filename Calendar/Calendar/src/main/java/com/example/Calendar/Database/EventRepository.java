b307bd31d349ee7d291e38134978fb239e7bcb89
Samuele <139059718+SanCatallo@users.noreply.github.com>
2023-12-12 16:17:13 +0100

    EventRepository.java

--- Calendar/Calendar/src/main/java/com/example/Calendar/Database/EventRepository.java
+++ Calendar/Calendar/src/main/java/com/example/Calendar/Database/EventRepository.java
@@ -1,9 +1,9 @@
 package com.example.Calendar.Database;
 
-import jdk.jfr.Event;
+import com.example.Calendar.Models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Integer> {
 }
